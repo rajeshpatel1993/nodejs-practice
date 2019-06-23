f6f678db05dda41b3636c8bf92f602cfc2b4c9e3
rajesh <rajesh@tatrasdata.com>
2019-06-23 17:55:39 +0530

    done with templates

--- app.js
+++ app.js
@@ -6,18 +6,25 @@ const bodyParser = require('body-parser');
 const rootDir = require('./util/path');
 
 const app = express();
-const adminRoutes = require('./routes/admin');
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 
 app.use((req,res,next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
    // res.status(404).send('<h1>Page Not Found</h1>');
+   res.status(404).render('404',{pageTitle: "404 Page Not found"});
 });
 
 
