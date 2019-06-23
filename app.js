2aa2370f30f2466bb93b8ca761b926d4f9f5a461
rajesh <rajesh@tatrasdata.com>
2019-06-23 18:02:47 +0530

    cleanup

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(shopRoutes);
 app.use((req,res,next) => {
     // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
    // res.status(404).send('<h1>Page Not Found</h1>');
-   res.status(404).render('404',{pageTitle: "404 Page Not found"});
+   res.status(404).render('404',{pageTitle: "404 Page Not found", path: ''});
 });
 
 
