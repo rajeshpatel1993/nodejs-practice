aa693410491d4bbb32c9f14985b385912a3bdd41
rajesh <rajesh@tatrasdata.com>
2019-09-01 22:50:22 +0530

    completed mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,20 +11,28 @@ exports.getAddProduct = (req, res, next) => {
     editing : false
   });
 };
-
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-
-  const product = new Product(title,imageUrl,price,description, null, req.user._id);
- 
-  product.save().then(result=>{ console.log('Product created');
-    res.redirect('/admin/products');
-  })
-  .catch(err => console.log(err));
-  
+  const product = new Product({
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl,
+    userId: req.user
+  });
+  product
+    .save()
+    .then(result => {
+      // console.log(result);
+      console.log('Created Product');
+      res.redirect('/admin/products');
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 
@@ -61,23 +69,27 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  const product = new Product(updatedTitle,updatedImageUrl, updatedPrice,updatedDesc,prodId);
-
- product.save() .then(result => {
-  
-    console.log('Updated product!')
-    res.redirect('/admin/products');
-  })
-  .catch(err=>console.log(err));
-
-  
+  Product.findById(prodId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
+    .then(result => {
+      console.log('UPDATED PRODUCT!');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-
-
-  Product.fetchAll()
+  Product.find()
+  // .select('title price -_id')
+  // .populate('userId', 'name')
   .then((products)=>{
+    console.log(products);
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
@@ -91,7 +103,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId).then(result => {
+  Product.findByIdAndRemove(prodId).then(result => {
     res.redirect('/admin/products');
     console.log('product destroyed');
   })
